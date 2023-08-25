74a42b8d96a50fe0f29872aa63174f729ad59ba6
João Victor da Silva <jv782063@gmail.com>
2023-08-24 21:28:59 -0300

    exercício 1 do nível 2

---
+++ nivel2/exerc1/README.md
@@ -0,0 +1,4 @@
+# N2 - exerc. 1
+
+- Escreva um programa que mostre um número em decimal, binário, e hexadecimal.
+
---
+++ nivel2/exerc1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  number := 100
+
+  fmt.Printf("%d\n%b\n%#x\n", number, number, number)
+}
+
