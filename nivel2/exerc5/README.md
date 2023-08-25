8ac3fe1a979797144e6cac1f2162ee824f31cbf0
João Victor da Silva <jv782063@gmail.com>
2023-08-24 21:47:16 -0300

    exercício 5 do nível 2

---
+++ nivel2/exerc5/README.md
@@ -0,0 +1,5 @@
+# N2 - exerc. 5
+
+Crie uma variável de tipo string utilizando uma raw string literal.
+Demonstre-a.
+
---
+++ nivel2/exerc5/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := `oie     estou distante
+  estou em outra linha
+              estou aquiiiii`
+
+  fmt.Println( a)
+}
+
