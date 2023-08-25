262e1f9e6c70ea1326223d23d5641a06fbe191c2
João Victor da Silva <jv782063@gmail.com>
2023-08-24 21:40:24 -0300

    exercício 3 do nível 2

---
+++ nivel2/exerc3/README.md
@@ -0,0 +1,5 @@
+# N2 - exerc. 3
+
+Crie constantes tipadas e não-tipadas.
+Demonstre seus valores.
+
---
+++ nivel2/exerc3/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  const a = 77
+  const b string = "Golang"
+
+  fmt.Println(a, b)
+}
+
