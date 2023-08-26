358d26310459360c3df0038e1ed6149ab181fd5d
João Victor da Silva <jv782063@gmail.com>
2023-08-26 11:16:03 -0300

    exercício 8 do nível 4

---
+++ nivel4/exerc8/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+  pessoas := map[string][]string{
+    "pessoa 1": {
+      "Programar", "Ler", "Estudar Golang",
+    },
+    "pessoa 2": {
+      "Jogar basquete", "Ir a igreja", "Comer",
+    },
+  }
+
+  for index, pessoa := range pessoas {
+    fmt.Printf("Indíce da pessoa %v \n", index)
+    fmt.Print("Seus hobbies: ")
+
+    for _, hobbie := range pessoa {
+      fmt.Print(hobbie, ", ")
+    }
+
+    fmt.Println("\n----------")
+  }
+}
+
